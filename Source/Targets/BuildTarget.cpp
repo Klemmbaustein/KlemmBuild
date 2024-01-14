b302463b0b1839979591c7fe1831ec81f13b5602
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2024-01-14 16:58:02 +0100

    fixed debug being true by default

--- Source/Targets/BuildTarget.cpp
+++ Source/Targets/BuildTarget.cpp
@@ -9,7 +9,7 @@ BuildTarget::BuildTarget()
 	Parameters.push_back(Parameter(Parameter::Type::Path, "outputPath", "", { "out", "output" }));
 	Parameters.push_back(Parameter(Parameter::Type::Enum, "configuration", "executable", { "config" }, {"executable", "staticLibrary", "sharedLibrary"}));
 	Parameters.push_back(Parameter(Parameter::Type::Enum, "optimization", "none", { "opt", "optimisation" }, { "none", "smallest", "fastest" }));
-	Parameters.push_back(Parameter(Parameter::Type::Boolean, "debug", "1", {}));
+	Parameters.push_back(Parameter(Parameter::Type::Boolean, "debug", "0", {}));
 	Parameters.push_back(Parameter(Parameter::Type::StringList, "defines", "", { "defs", "definitions"}));
 }
 
